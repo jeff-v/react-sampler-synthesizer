5f996adb6c1138ae7ffda1cbfef168543f64f8e1
Jeff Verhaaren <jeff.verhaaren@octanner.com>
2019-01-30 10:55:47 -0700

    began work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import styles from "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      source: {},
+      oscillator: {},
+      samples: [],
+      buttons: []
+    };
+  }
+
+  componentDidMount() {
+    const buttonArray = [];
+    for (let i = 0; i < 16; i++) {
+      buttonArray.push(<button className={i} />);
+    }
+
+    const AudioContext = window.AudioContext;
+    const audioCtx = new AudioContext();
+
+    const osc = audioCtx.createOscillator();
+    const gainNode = audioCtx.createGain();
+
+    osc.connect(gainNode);
+    gainNode.connect(audioCtx.destination);
+
+    this.setState({
+      oscillator: osc,
+      source: osc,
+      buttons: buttonArray
+    });
+  }
+
+  createSample = e => {
+    this.state.oscillator.start();
+    e.preventDefault();
+  };
+
   render() {
+    const { buttons } = this.state;
+    const samplerButtons = buttons.map(button => {
+      return <div className="box">{button}</div>;
+    });
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="">
+        <section className="wrapper">{samplerButtons}</section>
+        <button onClick={this.createSample}>Create Sample</button>
       </div>
     );
   }
