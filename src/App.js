7ec04895ccec4976ddebe5268f945f2a3e503a36
Jeff Verhaaren <jeff.verhaaren@octanner.com>
2019-01-30 14:27:37 -0700

    got different wave types working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import CreateSample from './components/CreateSample/CreateSample';
+import CreateSample from "./components/CreateSample/CreateSample";
 import "./App.css";
 
 class App extends Component {
@@ -12,37 +12,48 @@ class App extends Component {
     };
   }
 
-  playSample = (e) => {
+  playSample = e => {
     const sample = JSON.parse(e.target.value);
     const AudioContext = window.AudioContext;
     const audioCtx = new AudioContext();
 
-    const osc = audioCtx.createOscillator()
+    const osc = audioCtx.createOscillator();
     osc.detune.value = sample.detune;
-    osc.frequency.value = sample.frequency
+    osc.frequency.value = sample.frequency;
+    osc.type = sample.waveType
 
     const gainNode = audioCtx.createGain();
     osc.connect(gainNode);
     gainNode.connect(audioCtx.destination);
-    osc.start(0)
+    osc.start(0);
     gainNode.gain.value = sample.volume;
-    setTimeout(() => osc.stop(), sample.length * 1000)
-  }
+    setTimeout(() => osc.stop(), sample.length * 1000);
+  };
 
-  onSampleChange = (e) => {
+  onSampleChange = e => {
     this.setState(prevState => ({
       samples: [...prevState.samples, e]
-    }))
-  }
+    }));
+  };
 
   render() {
     const buttonArray = [];
     for (let i = 0; i < 16; i++) {
-      buttonArray.push(<button className={i} onClick={this.playSample} value={JSON.stringify(this.state.samples[i])} />);
+      buttonArray.push(
+        <button
+          className={i}
+          onClick={this.playSample}
+          value={JSON.stringify(this.state.samples[i])}
+        />
+      );
     }
 
     const samplerButtons = buttonArray.map((button, i) => {
-      return <div className="box" key={i}>{button}</div>;
+      return (
+        <div className="box" key={i}>
+          {button}
+        </div>
+      );
     });
 
     return (
